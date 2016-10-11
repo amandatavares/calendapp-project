65bf59d661957876602c3581d644702175f4a0c2
alcidesjunior <alcidesjunior.infor@gmail.com>
2016-10-10 22:23:08 -0300

    crud completo

--- modules/courses/courses.js
+++ modules/courses/courses.js
@@ -1,4 +1,5 @@
 jQuery.support.cors = true;
+//LIST ALL
 myApp.onPageInit('courses',function(page){
 $(document).ready(function(){
   console.log('course');
@@ -21,13 +22,14 @@ $(document).ready(function(){
         for (var i = 0; i < e.length; i++) {
           $('.list-course').
           append('<li>'+
-          '<a href="#" class="item-link">'+
+          '<a href="modules/courses/course.show.html?id='+e[i].id+'" class="item-link">'+
             '<div class="item-content">'+
                 '<div class="item-inner">'+
                     '<div class="item-title">'+
                       e[i].nome+
                     '</div>'+
                     '<div class="item-after">'+
+                      e[i].professor+
                     '</div>'+
                 '</div>'+
             '</div>'+
@@ -39,7 +41,7 @@ $(document).ready(function(){
     });
 });
 });
-
+//NEW
 myApp.onPageInit('courses-new',function(page){
   console.log('new course');
   $(document).ready(function(){
@@ -47,12 +49,11 @@ myApp.onPageInit('courses-new',function(page){
       $.ajax({
         "async": true,
         "crossDomain": true,
-        // contentType: "application/json;",
-        "url": "http://192.168.25.158:8000/disciplina/create",
+        "url": "http://192.168.25.158:8000/disciplina/create/",//page.query pega a variavel que eu passo ao clicar no item da lista
         "method": "POST",
         data:{
-          nome: $('#disciplina').val(),
-          professor: $('#professor').val()
+          nome:$('#disciplina').val(),
+          professor:$('#professor').val()
         },
         beforeSend:function(){
           myApp.showPreloader();
@@ -62,13 +63,90 @@ myApp.onPageInit('courses-new',function(page){
 
         },
         success:function(e){
-          myApp.alert(e,"Aviso");
+          $('#disciplina').val(e.nome);
+          $('#professor').val(e.professor);
+          mainView.router.loadPage('modules/courses/courses.html');
         }
       });
     }
-
     $('#btn_create_course').click(function(){
       create();
+    });
+
+  });
+});
+//SHOW
+myApp.onPageInit('courses-show',function(page){
+  $(document).ready(function(){
+    remove=function(){//
+      $.ajax({
+        "async": true,
+        "crossDomain": true,
+        "url": "http://192.168.25.158:8000/disciplina/delete/"+page.query.id,//page.query pega a variavel que eu passo ao clicar no item da lista
+        "method": "POST",
+        beforeSend:function(){
+          myApp.showPreloader();
+        },
+        complete:function(){
+          myApp.hidePreloader();
+        },
+        success:function(e){
+          myApp.hidePreloader();
+          myApp.alert(e,'Aviso');
+          mainView.router.loadPage('modules/courses/courses.html');
+        }
+      });
+    };
+    //listar no formulario de editar
+    $.ajax({
+      "async": true,
+      "crossDomain": true,
+      "url": "http://192.168.25.158:8000/disciplina/show/"+page.query.id,//page.query pega a variavel que eu passo ao clicar no item da lista
+      "method": "POST",
+      beforeSend:function(){
+        myApp.showPreloader();
+      },
+      complete:function(){
+        myApp.hidePreloader();
+
+      },
+      success:function(e){
+        $('#disciplina').val(e.nome);
+        $('#professor').val(e.professor);
+      }
+    });
+
+    //função pra atualizar
+    update=function(){
+      $.ajax({
+        "async": true,
+        "crossDomain": true,
+        //id é o nome da minha variável passada em
+        //'<a href="modules/courses/course.show.html?id='+e[i].id+'" class="item-link">'+
+        "url": "http://192.168.25.158:8000/disciplina/update/"+page.query.id,//page.query pega a variavel que eu passo ao clicar no item da lista
+        "method": "POST",
+        data:{
+          nome:$('#disciplina').val(),
+          professor:$('#professor').val()
+        },
+        beforeSend:function(){
+          myApp.showPreloader();
+        },
+        complete:function(){
+          myApp.hidePreloader();
+        },
+        success:function(e){
+          myApp.hidePreloader();
+          myApp.alert(e,'Aviso');
+          mainView.router.loadPage('modules/courses/courses.html');
+        }
+      });
+    }
+    $('#btn_update_course').click(function(){
+      update();
+    });
+    $('#btn_delete_course').click(function(){
+      remove();
     });
   });
 });
