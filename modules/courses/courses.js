1cd5e933a6110b06cb48493d7d117885be973484
alcidesjunior <alcidesjunior.infor@gmail.com>
2016-10-10 21:26:49 -0300

    add ok

--- modules/courses/courses.js
+++ modules/courses/courses.js
@@ -13,12 +13,62 @@ $(document).ready(function(){
       },
       complete:function(){
         myApp.hidePreloader();
-        $('.list-course').empty().
-        append('<li>Meu nome aqui</li>');
+
       },
       success:function(e){
         console.log(e);
+        $('.list-course').empty();
+        for (var i = 0; i < e.length; i++) {
+          $('.list-course').
+          append('<li>'+
+          '<a href="#" class="item-link">'+
+            '<div class="item-content">'+
+                '<div class="item-inner">'+
+                    '<div class="item-title">'+
+                      e[i].nome+
+                    '</div>'+
+                    '<div class="item-after">'+
+                    '</div>'+
+                '</div>'+
+            '</div>'+
+            '</a>'+
+          '</li>');
+
+        }
       }
     });
 });
+});
+
+myApp.onPageInit('courses-new',function(page){
+  console.log('new course');
+  $(document).ready(function(){
+    create=function(){
+      $.ajax({
+        "async": true,
+        "crossDomain": true,
+        // contentType: "application/json;",
+        "url": "http://192.168.25.158:8000/disciplina/create",
+        "method": "POST",
+        data:{
+          nome: $('#disciplina').val(),
+          professor: $('#professor').val()
+        },
+        beforeSend:function(){
+          myApp.showPreloader();
+        },
+        complete:function(){
+          myApp.hidePreloader();
+
+        },
+        success:function(e){
+          myApp.alert(e,"Aviso");
+        }
+      });
+    }
+
+    $('#btn_create_course').click(function(){
+      create();
+    });
+  });
 });
